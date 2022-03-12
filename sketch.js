62d9f459cb7724f81d2f438996f0ba25d2ca24e6
C-mhin <101394136+C-mhin@users.noreply.github.com>
2022-03-12 15:32:04 +0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,68 @@
+let N = 6;
+let model = {};
+
+function setup() {
+  noCanvas(); // use DOM instead
+  loadMarkovModel();
+  generate();
+}
+
+function generate(minLen = 50, maxLen = 200) {
+  // pick a random capital as start
+  let result = random(Object.keys(model).filter((k) => /^[A-Z]/.test(k)));
+
+  // loop until we hit a full-stop or maxLen
+  while (result.length < maxLen) {
+    // get the last N-1 chars
+    let seq = result.slice(-(N - 1));
+
+    // look it up the model
+    let options = model[seq];
+
+    // pick a random next char
+    let next = random(options);
+    result += next; // add it to our string
+
+    // end if we find a '.' and have passed minLen
+    if (
+      (result.length >= minLen && result.endsWith(".")) ||
+      result.endsWith("!") ||
+      result.endsWith("?")
+    )
+      break;
+  }
+
+  // add the result to our span
+  let span = document.getElementById("content");
+  span.innerText += " " + result;
+}
+
+function loadMarkovModel() {
+  let text = song1Data.lyrics.join(" ");
+
+  for (let i = 0; i < text.length; i++) {
+    let key = text.substring(i, i + (N - 1));
+    let next = text[i + (N - 1)];
+    if (!model[key]) model[key] = [];
+    model[key].push(next);
+  }
+
+  text = song2Data.lyrics.join(" ");
+
+  for (let i = 0; i < text.length; i++) {
+    let key = text.substring(i, i + (N - 1));
+    let next = text[i + (N - 1)];
+    if (!model[key]) model[key] = [];
+    model[key].push(next);
+  }
+
+  text = song3Data.lyrics.join(" ");
+  for (let i = 0; i < text.length; i++) {
+    let key = text.substring(i, i + (N - 1));
+    let next = text[i + (N - 1)];
+    if (!model[key]) model[key] = [];
+    model[key].push(next);
+  }
+
+  console.log(model);
+}
