f5019941df3e1ef28d12e10120d8a9ed07fec26d
C-mhin <tc00103100@gmail.com>
2022-03-13 15:25:22 +0800

    Added a new function adjust()

--- sketch.js
+++ sketch.js
@@ -7,6 +7,29 @@ function setup() {
   generate();
 }
 
+function adjust(input) {
+  if (input) {
+    if (N >= 12) {
+      N = 6;
+    } else {
+      N++;
+    }
+  } else {
+    if (N <= 6) {
+      N = 12;
+    } else {
+      N--;
+    }
+  }
+  let span = document.getElementById("N");
+  span.innerText = N;
+
+  span = document.getElementById("content");
+  span.innerText = "";
+  loadMarkovModel();
+  generate();
+}
+
 function generate(minLen = 50, maxLen = 200) {
   // pick a random capital as start
   let result = random(Object.keys(model).filter((k) => /^[A-Z]/.test(k)));
